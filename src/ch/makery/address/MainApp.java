9068730c0a574640d81609fbdabb2133a935ffae
Vanessa Panosso <panossovanessa@gmail.com>
2016-03-26 20:10:24 -0300

    Parte 5

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -1,10 +1,20 @@
 package ch.makery.address;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.controlsfx.dialog.Dialogs;
 
 import ch.makery.address.model.Person;
+import ch.makery.address.model.PersonListWrapper;
 import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
+import ch.makery.address.view.RootLayoutController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -65,19 +75,35 @@ public class MainApp extends Application {
     /**
      * Inicializa o root layout (layout base).
      */
+    /**
+     * Inicializa o root layout e tenta carregar o último arquivo
+     * de pessoa aberto.
+     */
     public void initRootLayout() {
         try {
             // Carrega o root layout do arquivo fxml.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            loader.setLocation(MainApp.class
+                    .getResource("view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
-            // Mostra a scene (cena) contendo oroot layout.
+            // Mostra a scene (cena) contendo o root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+
+            // Dá ao controller o acesso ao main app.
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // Tenta carregar o último arquivo de pessoa aberto.
+        File file = getPersonFilePath();
+        if (file != null) {
+            loadPersonDataFromFile(file);
         }
     }
 
@@ -147,6 +173,101 @@ public class MainApp extends Application {
         } catch (IOException e) {
             e.printStackTrace();
             return false;
+        }
+    }
+ 
+    /**
+     * Retorna o arquivo de preferências da pessoa, o último arquivo que foi aberto.
+     * As preferências são lidas do registro específico do SO (Sistema Operacional). 
+     * Se tais prefêrencias não puderem  ser encontradas, ele retorna null.
+     * 
+     * @return
+     */
+    public File getPersonFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Define o caminho do arquivo do arquivo carregado atual. O caminho é persistido no
+     * registro específico do SO (Sistema Operacional).
+     * 
+     * @param file O arquivo ou null para remover o caminho
+     */
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
+    }
+    
+    /**
+     * Carrega os dados da pessoa do arquivo especificado. A pessoa atual
+     * será substituída.
+     * 
+     * @param file
+     */
+    public void loadPersonDataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
+
+            // Reading XML from the file and unmarshalling.
+            PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+            personData.clear();
+            personData.addAll(wrapper.getPersons());
+
+            // Save the file path to the registry.
+            setPersonFilePath(file);
+
+        } catch (Exception e) { // catches ANY exception
+            Dialogs.create()
+                    .title("Erro")
+                    .masthead("Não foi possível carregar dados do arquivo:\n" 
+                              + file.getPath()).showException(e);
+        }
+    }
+
+    /**
+     * Salva os dados da pessoa atual no arquivo especificado.
+     * 
+     * @param file
+     */
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(PersonListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            // Envolvendo nossos dados da pessoa.
+            PersonListWrapper wrapper = new PersonListWrapper();
+            wrapper.setPersons(personData);
+
+            // Enpacotando e salvando XML  no arquivo.
+            m.marshal(wrapper, file);
+
+            // Saalva o caminho do arquivo no registro.
+            setPersonFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Dialogs.create().title("Erro")
+                    .masthead("Não foi possível salvar os dados do arquivo:\n" 
+                              + file.getPath()).showException(e);
         }
     }
     
